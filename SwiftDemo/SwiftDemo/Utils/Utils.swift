f21742f8f248b1db4d268d02d55b32ec07e87d73
youdone-ndl <nidongle@youdone.net>
2020-11-12 18:27:32 +0800

    笔记

--- SwiftDemo/SwiftDemo/Utils/Utils.swift
+++ SwiftDemo/SwiftDemo/Utils/Utils.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+public typealias SuccessCallback = (() -> Void)
+public typealias GenericCallback<T> = ((T) -> Void)
+public typealias JSONCallback = GenericCallback<JSONDictionary>
+public typealias StringCallback = GenericCallback<String>
+public typealias ErrorCallback = GenericCallback<Error>
+
 // 全局方法
 public func delay(by delayTime: TimeInterval, qosClass: DispatchQoS.QoSClass? = nil, _ closure: @escaping () -> Void) {
     let dispatchQueue = qosClass != nil ? DispatchQueue.global(qos: qosClass!) : .main
